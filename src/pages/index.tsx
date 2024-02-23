631f9408b882a76dbeed6353625534c623a680ca
Punith Gowda N <punithgowdanpg@gmail.com>
2024-02-23 12:53:09 +0530

    updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import Layout from "../components/Layout";
 import BasicMeta from "../components/meta/BasicMeta";
 import OpenGraphMeta from "../components/meta/OpenGraphMeta";
